54c6b398e6e1b39cb6a23c02594bb01682e52ee8
Kryssie <christabeladamu2@gmail.com>
2026-05-11 10:58:39 +0100

    add authentication

--- app.js
+++ app.js
@@ -1,253 +1,290 @@
-    const register = document.getElementById("register");
-    const login = document.getElementById("login");
-    const logout = document.getElementById("logout");
-    const send = document.getElementById("send");
-    const username = document.getElementById("username");
-    const password = document.getElementById("password");
-    const message = document.getElementById("message");
-    const auth = document.getElementById("auth");
-    const chatSection = document.getElementById("chatSection");
-    const chat = document.getElementById("chat");
+const register = document.getElementById("register");
+const login = document.getElementById("login");
+const logout = document.getElementById("logout");
+const send = document.getElementById("send");
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const message = document.getElementById("message");
+
+const auth = document.getElementById("auth");
+const chatSection = document.getElementById("chatSection");
+
+const chat = document.getElementById("chat");
 
 
 
-    let users =
-      JSON.parse(localStorage.getItem("users")) || [];
+let users =
+  JSON.parse(localStorage.getItem("users")) || [];
 
-    let messages =
-      JSON.parse(localStorage.getItem("messages")) || [];
+let messages =
+  JSON.parse(localStorage.getItem("messages")) || [];
 
-    let currentUser =
-      JSON.parse(localStorage.getItem("currentUser"));
-
-
-    function renderMessages() {
-
-      chat.innerHTML = "";
+let currentUser =
+  JSON.parse(localStorage.getItem("currentUser"));
 
 
-      messages.forEach((msg) => {
-        if (msg.sender === currentUser.username) {
-
-          chat.innerHTML += `
-
-            <div class="flex justify-end mb-3">
-
-              <div class="bg-blue-500 p-3 rounded-xl max-w-[70%]">
-
-                <h1 class="font-bold text-sm text-[#d8e0eb]">
-                  You
-                </h1>
-
-                <p class="mt-2">
-                  ${msg.text}
-                </p>
-
-              </div>
-
-            </div>
-
-          `;
-
-        } else {
-
-          chat.innerHTML += `
-
-            <div class="flex justify-start mb-3">
-
-              <div class="bg-[#202c33] p-3 rounded-xl max-w-[70%]">
-
-                <h1 class="font-bold text-sm text-[#0070fa]">
-                  ${msg.sender}
-                </h1>
-
-                <p class="mt-2">
-                  ${msg.text}
-                </p>
-
-              </div>
-
-            </div>
-
-          `;
-
-        }
-
-      });
 
 
-    }
+function renderMessages() {
+
+  chat.innerHTML = "";
 
 
-    function checkLogin() {
+  messages.forEach((msg) => {
 
-      if (currentUser) {
+    if (msg.sender === currentUser.username) {
 
-        auth.classList.add("hidden");
+      chat.innerHTML += `
 
-        chatSection.classList.remove("hidden");
+        <div class="flex justify-end mb-3">
 
-        renderMessages();
+          <div class="bg-blue-500 p-3 rounded-xl max-w-[70%]">
 
-      } else {
+            <h1 class="font-bold text-sm text-[#d8e0eb]">
+              You
+            </h1>
 
-        auth.classList.remove("hidden");
+            <p class="mt-2">
+              ${msg.text}
+            </p>
 
-        chatSection.classList.add("hidden");
+          </div>
 
-      }
+        </div>
+
+      `;
+
+    } else {
+
+      chat.innerHTML += `
+
+        <div class="flex justify-start mb-3">
+
+          <div class="bg-[#202c33] p-3 rounded-xl max-w-[70%]">
+
+            <h1 class="font-bold text-sm text-[#0070fa]">
+              ${msg.sender}
+            </h1>
+
+            <p class="mt-2">
+              ${msg.text}
+            </p>
+
+          </div>
+
+        </div>
+
+      `;
 
     }
 
+  });
 
-    register.addEventListener("click", (e) => {
+}
 
-      e.preventDefault();
 
 
 
+function checkLogin() {
 
-      if (
-        username.value === "" ||
-        password.value === ""
-      ) return;
+  if (currentUser) {
 
+    auth.classList.add("hidden");
 
+    chatSection.classList.remove("hidden");
 
+    renderMessages();
 
+  } else {
 
-      users.push({
+    auth.classList.remove("hidden");
 
-        username: username.value,
-        password: password.value
+    chatSection.classList.add("hidden");
 
-      });
+  }
 
+}
 
 
 
 
-      localStorage.setItem(
-        "users",
-        JSON.stringify(users)
-      );
+register.addEventListener("click", (e) => {
 
+  e.preventDefault();
 
 
 
 
-      alert("Account Created");
+  if (
+    username.value === "" ||
+    password.value === ""
+  ) return;
 
 
 
 
+  let userExists = users.find((user) => {
+    return user.username === username.value;
+  });
 
-      username.value = "";
-      password.value = "";
 
-    });
 
 
+  if (userExists) {
+    alert("User already exists");
+    return;
+  }
 
 
 
 
+  users.push({
 
-    login.addEventListener("click", (e) => {
+    username: username.value,
+    password: password.value
 
-      e.preventDefault();
+  });
 
 
 
 
-      let foundUser = users.find((user) => {
+  localStorage.setItem(
+    "users",
+    JSON.stringify(users)
+  );
 
-        return (
-          user.username === username.value &&
-          user.password === password.value
-        );
 
-      });
 
 
+  alert("Account Created");
 
 
 
-      if (foundUser) {
 
-        localStorage.setItem(
-          "currentUser",
-          JSON.stringify(foundUser)
-        );
+  username.value = "";
+  password.value = "";
 
+});
 
 
 
 
-        currentUser = foundUser;
 
 
+login.addEventListener("click", (e) => {
 
+  e.preventDefault();
 
 
-        checkLogin();
 
-      } else {
 
-        alert("Invalid Details");
+  let foundUser = users.find((user) => {
 
-      }
+    return (
+      user.username === username.value &&
+      user.password === password.value
+    );
 
-    });
+  });
 
 
-    send.addEventListener("click", () => {
 
-      if (message.value === "") return;
 
+  if (foundUser) {
 
-      messages.push({
+    localStorage.setItem(
+      "currentUser",
+      JSON.stringify(foundUser)
+    );
 
-        sender: currentUser.username,
-        text: message.value
 
-      });
 
 
-      localStorage.setItem(
-        "messages",
-        JSON.stringify(messages)
-      );
+    currentUser = foundUser;
 
 
-      renderMessages();
 
-
-      message.value = "";
-
-    });
-
-
-    logout.addEventListener("click", () => {
-
-      localStorage.removeItem("currentUser");
-
-      currentUser = null;
-
-      checkLogin();
-
-    });
-
-
-    window.addEventListener("storage", () => {
-
-      messages =
-        JSON.parse(localStorage.getItem("messages")) || [];
-
-      renderMessages();
-
-    });
 
     checkLogin();
 
-  
+  } else {
+
+    alert("Invalid Details");
+
+  }
+
+});
+
+
+
+
+
+
+send.addEventListener("click", () => {
+
+  if (message.value === "") return;
+
+
+
+
+  messages.push({
+
+    sender: currentUser.username,
+    text: message.value
+
+  });
+
+
+
+
+  localStorage.setItem(
+    "messages",
+    JSON.stringify(messages)
+  );
+
+
+
+
+  renderMessages();
+
+
+
+
+  message.value = "";
+
+});
+
+
+
+
+
+
+logout.addEventListener("click", () => {
+
+  localStorage.removeItem("currentUser");
+
+  currentUser = null;
+
+  checkLogin();
+
+});
+
+
+
+
+
+
+window.addEventListener("storage", () => {
+
+  messages =
+    JSON.parse(localStorage.getItem("messages")) || [];
+
+  renderMessages();
+
+});
+
+
+
+
+checkLogin();
